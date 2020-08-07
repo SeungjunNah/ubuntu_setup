67d230d41ade0da4146e9a2637a50e164953234f
Seungjun Nah <seungjun.nah@gmail.com>
2020-08-07 23:35:45 +0900

    add pylint, automate installation

--- conda_addon.sh
+++ conda_addon.sh
@@ -1,5 +1,5 @@
 conda install requests -y
 conda install imageio tqdm matplotlib -y
 pip install opencv-python
-conda install -c conda-forge scikit-image
-conda install mypy -y   # linting with mypy can detect torch elements
+conda install -c conda-forge scikit-image -y
+conda install pylint mypy -y   # linting with mypy can detect torch elements
